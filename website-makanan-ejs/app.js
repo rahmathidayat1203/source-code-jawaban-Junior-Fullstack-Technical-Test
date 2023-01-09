4bac6a99c7bcbf8fff1ca0b376a4208f22245a87
rahmathidayat1203 <rh63800@gmail.com>
2023-01-09 23:23:58 +0700

    updating web makanan barokah

--- website-makanan-ejs/app.js
+++ website-makanan-ejs/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
 app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
 });
 
 app.listen(port, () => {
